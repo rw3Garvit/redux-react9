6f363fc8442825cd56c5e2c59a9a3a8873e8121c
Garvit <rw3.garvit.rd@gmail.com>
2024-06-10 10:06:20 +0530

    useSelector

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import { useEffect } from "react";
 import "./App.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { GET_PRODUCT_PENDING } from "./redux-saga/admin/action";
 
 function App() {
   let dispatch = useDispatch();
+
+  //fetch data from store
+  let result = useSelector((state) => state.adminReducer);
 
   useEffect(() => {
     // action
     dispatch({ type: GET_PRODUCT_PENDING });
   }, []);
 
-  return <div className="App"></div>;
+  if (result.isLoading) {
+    return <h1>Loading....</h1>;
+  }
+
+  return (
+    <div className="App">
+      {result.product.map((val, index) => {
+        return (
+          <>
+            <h1>{val.id}</h1>
+            <h2>{val.productName}</h2>
+            <h3>{val.price}</h3>
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
